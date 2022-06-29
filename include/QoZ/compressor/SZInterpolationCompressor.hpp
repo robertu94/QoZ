52cbdb38c6bb9dcfd82d6adac9a378e0f94302ce
meso272 <liujinyang1201@126.com>
2022-06-28 19:28:36 -0700

    blockwise

--- include/QoZ/compressor/SZInterpolationCompressor.hpp
+++ include/QoZ/compressor/SZInterpolationCompressor.hpp
@@ -1488,7 +1488,7 @@ namespace QoZ {
 
         double block_interpolation_1d_cross(T *data, size_t begin, size_t end, size_t stride,
                                       const std::string &interp_func,
-                                      const PredictorBehavior pb,int tuning=0,size_t cross_block=0,size_t axis_begin,size_t axis_stride) {//cross block: 0: no cross 1: only front-cross x: cross until x
+                                      const PredictorBehavior pb,int tuning=0,size_t cross_block=0,size_t axis_begin=0,size_t axis_stride=0) {//cross block: 0: no cross 1: only front-cross x: cross until x
 
             size_t n = (end - begin) / stride + 1;
             if (n <= 1) {
