4a02eb810009d435155d7d679f2e031182aa581c
meso272 <liujinyang1201@126.com>
2022-08-31 15:39:10 -0700

    wavelet

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -3,8 +3,10 @@
 
 #include "QoZ/compressor/SZInterpolationCompressor.hpp"
 #include "QoZ/compressor/deprecated/SZBlockInterpolationCompressor.hpp"
+extern "C"{
 #include "QoZ/preprocessor/Wavelet.hpp"
 #include "QoZ/preprocessor/Wavelet2.hpp"
+}
 #include "QoZ/quantizer/IntegerQuantizer.hpp"
 #include "QoZ/lossless/Lossless_zstd.hpp"
 #include "QoZ/utils/Iterator.hpp"
@@ -95,9 +97,11 @@ void SZ_decompress_Interp(const QoZ::Config &conf, char *cmpData, size_t cmpSize
     if(conf.wavelet){
         //QoZ::Wavelet<T,N> wlt;
         //wlt.postProcess(decData,conf.num);
-        extern "C"{
+       
+
         postProcess_t(reinterpret_cast<float *>(decData),conf.num);
-        }
+        
+
 
     }
     
@@ -3274,9 +3278,9 @@ char *SZ_compress_Interp_lorenzo(QoZ::Config &conf, T *data, size_t &outSize) {
     if(conf.wavelet){
        // QoZ::Wavelet<T,N> wlt;
         //wlt.preProcess(data,conf.num);
-        extern "C"{
+        
         preProcess_t(reinterpret_cast<float *>(data),conf.num);
-        }
+        
 
     }
     
