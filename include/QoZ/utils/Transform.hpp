bb8ed6ac1301a523a2b2ffab0d6ec3d85fdc817b
meso272 <liujinyang1201@126.com>
2022-09-27 01:58:27 -0700

    transforms for wave coeff

--- include/QoZ/utils/Transform.hpp
+++ include/QoZ/utils/Transform.hpp
@@ -17,11 +17,16 @@ namespace QoZ {
     }
     template<class T>
     inline T logit(T x){
+        if(x>=1)
+            x=1-1e-10
+        else if (x<=0)
+            x=1e-10
         return log( x /( (double)(1.0)-x ) );
     }
    
     template<class T>
     inline T tanh(T x){
+
         if(x>=0){
             double t=exp(-2*x);
             return ((double)(1.0)-t)/((double)(1.0)+t);
@@ -34,6 +39,10 @@ namespace QoZ {
     }
     template<class T>
     inline T arctanh(T x){
+        if(x>=1)
+            x=1-1e-10
+        else if (x<=-1)
+            x=-1+1e-10
 
         return 0.5* ( log ( ( (double)(1.0)+x )/( (double)(1.0)-x ) ) );
     }
