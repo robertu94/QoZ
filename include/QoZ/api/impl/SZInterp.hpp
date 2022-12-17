80a4a4aadfda8603c8a321f009e30a0d983e3879
meso272 <liujinyang1201@126.com>
2022-12-17 01:26:45 -0800

    wavelet autotuning

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -1984,6 +1984,13 @@ char *SZ_compress_Interp_lorenzo(QoZ::Config &conf, T *data, size_t &outSize) {
             for(size_t i=0;i<conf.num;i++){
                 decData[i]=origdata[i]-decData[i];
             }
+            size_t count=0;
+            for(size_t i=0;i<conf.num;i++){
+                if(fabs(decData[i])>prewave_absErrorBound)
+                    count++;
+            }
+            std::cout<<count<<std::endl;
+
             //std::cout<<"origdatadel"<<std::endl;
             delete []origdata;
         }
