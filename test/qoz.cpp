bd14b65c669ca8c5104c5c097b8e99c50955fab0
meso272 <liujinyang1201@126.com>
2022-12-22 20:21:15 -0800

    conditioning

--- test/qoz.cpp
+++ test/qoz.cpp
@@ -162,7 +162,6 @@ void compress(char *inPath, char *cmpPath, QoZ::Config conf) {
     delete[]data;
     delete[]bytes;
 
-    std::cout<<"eeend"<<std::endl;
    
 }
 
@@ -174,7 +173,8 @@ void decompress(char *inPath, char *cmpPath, char *decPath,
     size_t cmpSize;
    
     auto cmpData = QoZ::readfile<char>(cmpPath, cmpSize);
-    std::cout<<cmpSize<<std::endl;
+    std::cout<<"woshinidie"<<std::endl;
+    
 
     QoZ::Timer timer(true);
     T *decData = SZ_decompress<T>(conf, cmpData.get(), cmpSize);
