99f0b2095d68e0b9ed41356d09955a19041031cc
meso272 <liujinyang1201@126.com>
2022-12-23 15:41:47 -0800

    coeff track

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -436,6 +436,8 @@ void SZ_decompress_Interp(const QoZ::Config &conf, char *cmpData, size_t cmpSize
         QoZ::Config newconf(conf.num);
 
         //newconf.blockSize=32768;
+        std::cout<<first<<std::endl;
+        std::cout<<second<<std::endl;
         outlier_decompress<T,N>(newconf,(char*)(cmpDataPos+first),second,offsets);
         
         //QoZ::writefile<T>("waved.qoz.dec.offset", offsets, conf.num); 
