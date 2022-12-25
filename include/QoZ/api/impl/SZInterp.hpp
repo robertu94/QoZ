9a8e48345196a4d79f3814837f2317f42dfcde47
meso272 <liujinyang1201@126.com>
2022-12-24 17:46:25 -0800

    tuningtest

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -1103,7 +1103,7 @@ std::pair<double,double> CompressTest(const QoZ::Config &conf,const std::vector<
                     if(testConfig.conditioning){
                         post_Condition<T,N>(cur_block.data(),per_block_ele_num,testConfig.block_metas[k]);
                     }
-                    /*
+                    
                     std::vector<T> offsets(per_block_ele_num);
                     
                     for(size_t i=0;i<per_block_ele_num;i++)
@@ -1117,7 +1117,7 @@ std::pair<double,double> CompressTest(const QoZ::Config &conf,const std::vector<
                     //std::cout<<oc_size<<std::endl;
                     for(size_t i=0;i<per_block_ele_num;i++)
                         cur_block[i]+=offsets[i];
-                    */
+
                 }
                 
 
@@ -1274,7 +1274,7 @@ std::pair<double,double> CompressTest(const QoZ::Config &conf,const std::vector<
             mse*=testConfig.waveletMseFix;
         else if(testConfig.wavelet>1)
             mse*=testConfig.waveletMseFix2;
-                    std::cout<<mse<<std::endl;
+                    //std::cout<<mse<<std::endl;
         metric=QoZ::PSNR(testConfig.rng,mse);
                     //std::cout<<metric<<std::endl;
                     //std::cout<<"---"<<std::endl;
