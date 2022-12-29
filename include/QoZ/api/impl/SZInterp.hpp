cb4b093833555d271122508037c1a02a658ac693
meso272 <liujinyang1201@126.com>
2022-12-29 14:11:07 -0800

    sperr waves

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -1072,13 +1072,13 @@ std::pair<double,double> CompressTest(const QoZ::Config &conf,const std::vector<
                            
     for (int k=0;k<num_sampled_blocks;k++){
         size_t sampleOutSize;
-        std::vector<T> cur_block;
+        std::vector<T> cur_block(testConfig.num);
         if(testConfig.wavelet==0 or waveleted_input.size()==0){
             std::cout<<"fuqindejian0"<<std::endl;
-            cur_block=std::copy(sampled_blocks[k].begin(),sampled_blocks[k].end());
+            std::copy(sampled_blocks[k].begin(),sampled_blocks[k].end(),cur_block.begin());
         }
         else{
-            cur_block=std::copy(waveleted_input[k].begin(),waveleted_input[k].end());
+            std::copy(waveleted_input[k].begin(),waveleted_input[k].end(),cur_block.begin());
             std::cout<<"fuqindejian1"<<std::endl;
 
         }
