04196961237f2957d96ed504d3661950289f67a9
meso272 <liujinyang1201@126.com>
2022-12-29 17:11:01 -0800

    sperr waves

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -2056,8 +2056,8 @@ double Tuning(QoZ::Config &conf, T *data){
                     std::vector<size_t> coeffs_size;
                     size_t coeffs_num=1;
                     for(size_t i=0;i<waveleted_input.size();i++){
-                        //std::cout<<waveleted_input[i].size();
-                        //std::cout<<conf.num<<std::endl;
+                        std::cout<<waveleted_input[i].size();
+                        std::cout<<conf.num<<std::endl;
                         T * coeffData=QoZ::external_wavelet_preprocessing<T,N>(waveleted_input[i].data(), conf.dims, conf.num, wave_idx, conf.pid,false,coeffs_size);
                         if(i==0){     
                             //std::cout<<coeffs_size[0]<<std::endl;
