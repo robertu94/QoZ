fcbefd81e08439fc95bbcdf36f3c956ef6cdfee7
meso272 <liujinyang1201@126.com>
2023-01-19 03:41:08 -0800

    readme

--- include/QoZ/api/impl/SZInterp.hpp
+++ include/QoZ/api/impl/SZInterp.hpp
@@ -2423,12 +2423,15 @@ char *SZ_compress_Interp_lorenzo(QoZ::Config &conf, T *data, size_t &outSize) {
         if(conf.wavelet>1){
             //read a coeff array and a size information array
             if(conf.pyBind){
+                std::cout<<"inits"<<std::endl;
                 std::string HOME = "/home/jinyang.liu";
                 py::module_::import("sys").attr("path").attr("append")(HOME + "/QoZ/include/QoZ/preprocessor");
                 pyModule = py::module_::import("pywt_wrapper");
+                std::cout<<"initf"<<std::endl;
 
                 
                 coeffData=QoZ::pybind_wavelet_preprocessing<T,N>(data, conf.dims, conf.num,pyModule, metadata,conf.wavelet, false, coeffs_size);
+                std::cout<<"waved"<<std::endl;
             }
             else{
                 coeffData=QoZ::external_wavelet_preprocessing<T,N>(data, conf.dims, conf.num, conf.wavelet, conf.pid, false, coeffs_size);
